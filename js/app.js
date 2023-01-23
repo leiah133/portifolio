c772f7517c46a27586f21bed443cd435239c5da5
Léia Oliver <106698637+Leiah133@users.noreply.github.com>
2023-01-22 21:28:38 -0300

    ajustes 1.2

--- js/app.js
+++ js/app.js
@@ -27,6 +27,7 @@ class MobileNavbar {
 
   addClickEvent() {
     this.mobileMenu.addEventListener("click", this.handleClick);
+    this.navLinks.forEach(link => link.addEventListener("click", this.handleClick));
   }
 
   init() {
